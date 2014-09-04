885480e36d2c4822dc9043d6de7df4ea25e04ebc
herberu <herber.urbina@xoom.com>
2014-09-04 15:38:28 -0600

    Create animal.rb

---
+++ animal.rb
@@ -0,0 +1,21 @@
+def pig message, thoughts = '\\', eyes = 'oo', ears = "^"
+%[
+    #{message}
+          \\
+            \\
+                ^__^
+               ( o o )------_
+               0___          \\@/
+                    \\_______/
+        M_M_M_M_M__  w w   w w  __M_M_M_M_
+
+        ]
+end
+
+
+message = %[
+_________________
+Welcome to Ruby!
+-----------------]
+
+puts pig message
